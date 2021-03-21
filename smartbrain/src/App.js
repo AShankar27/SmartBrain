7aa33ec5409b93001fa2cee29c9d34cb7492dc07
Abhishek <abhi.ashwin@gmail.com>
2021-03-21 14:42:32 +0530

    first look

---
+++ smartbrain/src/App.js
@@ -0,0 +1,35 @@
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 400,
+      }
+    }
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+            {/*<FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
