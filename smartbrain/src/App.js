6c7336749c41c585c6828e0544a65fd46f603e54
Abhishek <abhi.ashwin@gmail.com>
2021-03-28 17:55:17 +0530

    remove bounding box bug

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementByID('inputImage');
+    const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
